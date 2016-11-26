a7bd1b6c28ae2b2e8cc67329a3c5801fe7d8b1b0
luannoaa <luan.wtf2@hotmail.com>
2016-11-26 02:00:13 -0200

    Update updateID.sql

--- updateID.sql
+++ updateID.sql
@@ -1,37 +1,10 @@
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
---
--- Host: localhost    Database: updateid
--- ------------------------------------------------------
--- Server version	5.7.16-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Dumping data for table `tbbotresposta`
---
-
-LOCK TABLES `tbbotresposta` WRITE;
-/*!40000 ALTER TABLE `tbbotresposta` DISABLE KEYS */;
-INSERT INTO `tbbotresposta` VALUES (13,'Luan','/megasena','11 - 17 - 31 - 51 - 55 - 58','481287510'),(12,'Luan','/megasena','11 - 26 - 39 - 45 - 47 - 59','481287513'),(11,'Luan','/megasena','04 - 15 - 24 - 33 - 41 - 44','481287515'),(10,'Luan','/megasena','02 - 04 - 15 - 32 - 45 - 48','481287516'),(9,'Luan','/megasena','29 - 42 - 43 - 47 - 50 - 51','481287517'),(8,'Luan','/megasena','23 - 24 - 30 - 35 - 45 - 48','481287518'),(7,'Luan','/megasena','16 - 35 - 36 - 37 - 49 - 50','481287519'),(6,'Luan','/megasena','13 - 17 - 36 - 37 - 42 - 43','481287520'),(5,'Luan','/megasena','18 - 20 - 34 - 41 - 48 - 60','481287521'),(4,'Ana','/megasena','04 - 10 - 20 - 49 - 50 - 53','481287523'),(3,'A','/megasena','01 - 12 - 23 - 27 - 29 - 40','481287528'),(2,'Luan','/megasena','02 - 16 - 27 - 31 - 32 - 52','481287529'),(1,'Luan','/megasena','08 - 24 - 36 - 38 - 39 - 45','481287531');
-/*!40000 ALTER TABLE `tbbotresposta` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-11-26  1:46:11
+CREATE TABLE `tbbotresposta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `nm_comando` varchar(255) DEFAULT NULL,
+  `resposta` varchar(255) DEFAULT NULL,
+  `update_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`update_id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
