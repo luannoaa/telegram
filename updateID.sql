62bb4fba9420dae232ba39ffbae73272561aa2bd
luannoaa <luan.wtf2@hotmail.com>
2016-12-01 22:21:36 -0200

    normalização banco
    normalização banco

--- updateID.sql
+++ updateID.sql
@@ -1,10 +1,22 @@
+DROP TABLE IF EXISTS tbcomando;
+DROP TABLE IF EXISTS tbbotresposta;
+
+
+create table tbcomando(
+	id_comando int not null auto_increment,
+	nm_comando varchar(255) not null,
+	primary key (id_comando),
+	CONSTRAINT uk_tbcomando_nm_comando UNIQUE (nm_comando) 
+);
+
+
 CREATE TABLE `tbbotresposta` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(255) NOT NULL,
-  `nm_comando` varchar(255) DEFAULT NULL,
-  `resposta` varchar(255) DEFAULT NULL,
-  `update_id` varchar(255) NOT NULL,
-  PRIMARY KEY (`update_id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+   id_resposta int NOT NULL AUTO_INCREMENT,
+   id_comando INT NOT NULL,
+  first_name varchar(255) NOT NULL,
+  resposta varchar(255) DEFAULT NULL,
+  update_id varchar(255) NOT NULL,
+  PRIMARY KEY (id_resposta),
+  CONSTRAINT uk_tbresposta_update_id UNIQUE (update_id),
+  FOREIGN KEY (id_comando) references tbcomando(id_comando)
+);
